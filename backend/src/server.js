e226dbbbd39455f380a7846f6049d466f4e24735
snikehero <snikehero234@gmail.com>
2025-08-30 17:19:27 -0600

    First connect to db then start server

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
 //Middleware
 app.use(express.json()); //This middleware is used to parse JSON bodies: req.body
 //Our simple custom middleware to log request method and url
@@ -21,9 +20,14 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 
+connectDB().then (() => {  //Connect to DB first then start the server
 
-app.listen(5001, () => {
-    console.log("Server started on port: ",PORT);
+    app.listen(5001, () => {
+        console.log("Server started on port: ",PORT);
+    });
+}).catch((error) => {
+    console.error("Failed to connect to DB", error);
 });
+
 
 
