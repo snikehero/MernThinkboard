b4742fdf3a0b5eff06d1364064e781f2664a7f45
snikehero <snikehero234@gmail.com>
2025-08-30 17:16:55 -0600

    Added rate limit using upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB}  from "./config/db.js"; //To connect with the database
 import dotenv from "dotenv"; //For environment Variables
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
@@ -9,7 +10,15 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 //Middleware
-app.use(express.json()); //To parse JSON request bodies
+app.use(express.json()); //This middleware is used to parse JSON bodies: req.body
+//Our simple custom middleware to log request method and url
+app.use((req,res,next) => {
+    console.log(`Req method is ${req.method} & Req url is ${req.url}`);
+    next();
+})
+
+app.use(rateLimiter);
+
 app.use("/api/notes",notesRoutes);
 
 
