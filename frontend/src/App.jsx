c752b86bb4e3f5c6db6ba7ec3480c9f90d7e5c3b
snikehero <snikehero234@gmail.com>
2025-08-30 22:49:46 -0600

    fixed directive error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,6 @@ import toast from 'react-hot-toast'
 const App = () => {
   return (
     <div data-theme = "forest">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/create" element={<CreatePage/>} />
