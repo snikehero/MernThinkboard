ea886ce79b6c19e412fb8524ef90e24987b8cd1f
snikehero <snikehero234@gmail.com>
2025-08-31 17:39:59 -0600

    Added Update on Notes

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,120 @@
-
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router';
+import api from '../lib/axios'
+import toast from 'react-hot-toast';
+import { ArrowLeftIcon, LoaderIcon, TrashIcon } from 'lucide-react';
 const NoteDetailPage = () => {
+    const [note,setNote] = useState(null);
+    const [loading,setLoading] = useState(true);
+    const [saving,setSaving] = useState(false);
+
+    const navigate = useNavigate();
+    
+    const {id} = useParams();
+
+    useEffect (() => { //Fetching the note from the se
+      const fetchNote = async () => {
+            try {
+                const res = await api.get(`/notes/${id}`)
+                setNote(res.data);
+            } catch (error) {
+                console.log("Error in fetching note", error)
+                toast.error("Failed to fetch the note")
+            } finally {
+                setLoading(false)
+            }
+        };
+        fetchNote();
+    }, [id] );    
+  const handleDelete = async () => { // Function to handle the delete of a note
+    if (!window.confirm('Are you sure you want to delete this note?')) return;
+    try {
+          console.log("entro a handle")
+
+      await api.delete(`/notes/${id}`);
+      toast.success("Notes deleted");
+      navigate("/");
+    } catch (error) {
+      console.log("Error deleteing the note",note);
+      toast.error("Failed to delete note");
+    }
+  };
+    const handleSave = async () => {
+      if(!note.title.trim() || !note.content.trim()) {
+        toast.error("Please add a title or content")
+        return;
+      }
+      setSaving(true) 
+      try {
+        await api.put(`/notes/${id}`, note);
+        toast.success("Note updated succesfully");
+        navigate("/")
+      } catch (error) {
+        console.log("Error saving the note",note)
+        toast.error("Failed to update the note");
+      } finally {
+        setSaving(false);
+      }
+    };
+
+    if(loading) {
+      return (
+        <div className='min-h-screen bg-base-200 flex items-center justify-center'>
+          <LoaderIcon className='animate-spin size-10'/>
+        </div>
+      )
+    }
   return (
-    <div>NoteDetailPage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className="max-w-2xl mx-auto">
+          <div className='flex items-center justify-between mb-6'>
+            <Link to="/" className='btn btn-ghost'>
+              <ArrowLeftIcon className='h-5 w-5'/>
+              Back to notes
+            </Link>
+            <button onClick={handleDelete} className='btn btn-error btn-outline'>
+              <TrashIcon className='h-5 w-5'/>
+              Delete Note
+            </button>
+          </div>
+        
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+              <label className='label'>
+                <span className='label-text'>Title</span>
+              </label>
+              <input 
+                type= "text"
+                placeholder='Note title'
+                className='input input-bordered'
+                value={note.title}
+                onChange={(e) => setNote({...note,title:e.target.value})}
+              />
+              </div>  
+              <div className='form-control mb-4'>
+                <label className='label'>
+                  <span className='label-text'>Content</span>
+                </label>
+                <textarea
+                  placeholder='Write your note here...'
+                  className='textarea textarea-bordered h-32'
+                  value={note.content}
+                  onChange={(e) => setNote({...note, content: e.target.value})}
+                  />
+              </div>
+              <div className="card-actions justify-end">
+                <button className='btn btn-primary' disabled ={saving} onClick={handleSave}>
+                  {saving ? "Saving...": "Save changes"}
+                </button>
+              </div>
+            </div>   
+          </div>
+        </div>
+      </div>
+    </div>
   )
-}
+};
 
 export default NoteDetailPage
